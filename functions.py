07324fcc3cea13311663b23d45582da63e1a4207
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-04-06 00:24:05 +0200

    minor refactor

--- functions.py
+++ functions.py
@@ -12,9 +12,9 @@ def sigmoid_derivative(x):
 def cross_entropy(Y_hat, Y_data):
     # ensure that no log(0) is done
     delta = 1e-7
-    Y_hat += delta
-    Y_data = Y_data.astype(float)
-    Y_hat += delta
 
-    to_sum = Y_data * np.log(Y_hat) + (1 - Y_data) * np.log((1 - Y_hat)) + delta
+    to_sum = Y_data * np.log(Y_hat + delta) + (1 - Y_data) * np.log((1 - Y_hat + delta))
     return -np.sum(to_sum)
+
+def argmax(X):
+    pass
